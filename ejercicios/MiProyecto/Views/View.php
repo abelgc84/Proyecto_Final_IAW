d71040ef5ec3e34a06bb830bc1b9fc866652b644
abelgc84 <146434908+abelgc84@users.noreply.github.com>
2024-03-05 09:50:24 +0100

    restart

--- ejercicios/MiProyecto/Views/View.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class View {
-    public static function show($viewName, $data=null) {
-        include ("$viewName.php");
-    }
-}
--- ejercicios/MiProyecto/Views/usersview.php
+++
@@ -1,7 +0,0 @@
-<html>
-    <h1>Listado de usuarios</h1>
-</html>
-
-<?php
-    print_r($data);
-?>
