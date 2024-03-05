907788d6f759cdea0b20ba545c696d776635c134
abelgc84 <146434908+abelgc84@users.noreply.github.com>
2024-03-05 10:22:12 +0100

    restart desde 0

---
+++ views/Footer.php
@@ -0,0 +1,5 @@
+<html>
+    <footer>
+        <p>Copyright 2024. Todos los derechos reservados.</p>
+    </footer>
+</html>
---
+++ views/Header.php
@@ -0,0 +1,5 @@
+<html>
+    <header>
+    <li class="nav-item"><a href="index.php?controller=ProductController&action=getAllProducts" class="nav-link">Listar Productos</a></li>
+    </header>
+</html>
